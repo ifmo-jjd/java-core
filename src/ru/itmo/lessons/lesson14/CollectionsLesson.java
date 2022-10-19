9a9ac91cd3cf15a798e3886ee02bfbe113e26f33
Dasha <byrtosha@yandex.ru>
2022-10-19 22:32:17 +0300

    Занятие 14: коллекции

---
+++ src/ru/itmo/lessons/lesson14/CollectionsLesson.java
@@ -0,0 +1,11 @@
+package ru.itmo.lessons.lesson14;
+
+public class CollectionsLesson {
+    public static void main(String[] args) {
+        Student student1 = new Student(1, "Maxim", 17);
+        Student student2 = new Student(2, "Anna", 21);
+        Student student3 = new Student(3, "Paul", 23);
+        Student student4 = new Student(4, "Helen", 19);
+        Student student5 = new Student(4, "Helen", 19);
+    }
+}
