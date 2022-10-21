266267eb7ef370dee0d143e27883d8cc18a3dc5f
Dasha <byrtosha@yandex.ru>
2022-10-21 23:06:59 +0300

    Занятие 15: map

---
+++ src/ru/itmo/lessons/lesson15/MapLesson.java
@@ -0,0 +1,17 @@
+package ru.itmo.lessons.lesson15;
+
+public class MapLesson {
+
+    public static void main(String[] args) {
+
+
+
+        User user1 = new User("a", "a1", Role.ADMIN, 18);
+        User user2 = new User("b", "b1", Role.USER, 22);
+        User user3 = new User("c", "c1", Role.USER, 22);
+        User user4 = new User("d", "d1", Role.USER, 44);
+        User user5 = new User("f", "f1", Role.ADMIN, 18);
+
+
+    }
+}
