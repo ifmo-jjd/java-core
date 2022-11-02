a2c3c67d9894d88cfb0d2023919519430be85034
Dasha <byrtosha@yandex.ru>
2022-11-02 18:10:35 +0300

    Занятие 19: io, nio, nio.2

---
+++ src/ru/itmo/lessons/lesson19/IOFiles.java
@@ -0,0 +1,7 @@
+package ru.itmo.lessons.lesson19;
+
+public class IOFiles {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/ru/itmo/lessons/lesson19/NIOFiles.java
@@ -0,0 +1,53 @@
+package ru.itmo.lessons.lesson19;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+public class NIOFiles {
+
+    private static void readAllBytes(){
+        Path fileName = Path.of("nio-data.txt");
+
+        try(FileChannel inChannel = FileChannel.open(fileName)) {
+
+            long fileSize = inChannel.size();
+
+            ByteBuffer buffer = ByteBuffer.allocate((int) fileSize);
+            inChannel.read(buffer);
+            buffer.flip();
+
+            System.out.println(new String(buffer.array()));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void writeToFile(String data){
+        Path fileName = Path.of("nio-data.txt");
+
+        try (FileChannel channel = FileChannel.open(fileName,
+                StandardOpenOption.CREATE,
+                StandardOpenOption.APPEND)) {
+
+            byte[] strBytes = data.getBytes();
+            ByteBuffer buffer = ByteBuffer.allocate(strBytes.length);
+
+            buffer.put(strBytes);
+            buffer.flip();
+            channel.write(buffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        writeToFile("Данные для записи\n");
+        readAllBytes();
+    }
+}
