157bfb77e4e930dbc12d1f5227e7e7bb0a4a03be
Dasha <byrtosha@yandex.ru>
2022-11-09 17:54:44 +0300

    Занятие 21: передача данных по сети (IO пакет + Socket)

---
+++ src/ru/itmo/lessons/lesson21/Message.java
@@ -0,0 +1,46 @@
+package ru.itmo.lessons.lesson21;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class Message implements Serializable {
+    private String sender;
+    private String text;
+    private LocalDateTime dateTime;
+
+    public Message(String sender, String text) {
+        this.sender = sender;
+        this.text = text;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setDateTime() {
+        dateTime = LocalDateTime.now();
+    }
+
+
+    @Override
+    public String toString() {
+        return "SimpleMessage{" +
+                "sender='" + sender + '\'' +
+                ", text='" + text + '\'' +
+                ", dateTime=" + dateTime +
+                '}';
+    }
+
+}
