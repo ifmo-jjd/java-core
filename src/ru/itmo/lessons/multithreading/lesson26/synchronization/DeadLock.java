9c1abbdb2c5ba75bd0f15160054922cc8e686d29
Dasha <byrtosha@yandex.ru>
2022-11-21 22:39:10 +0300

    Занятие 26: взаимная блокировка ++

--- src/ru/itmo/lessons/multithreading/lesson26/synchronization/DeadLock.java
+++ src/ru/itmo/lessons/multithreading/lesson26/synchronization/DeadLock.java
@@ -3,20 +3,21 @@ package ru.itmo.lessons.multithreading.lesson26.synchronization;
 public class DeadLock {
     public static void main(String[] args) {
 
+        // взаимная блокировка
         Object object1 = new Object();
         Object object2 = new Object();
 
         Thread thread1 = new Thread(()->{
             System.out.println(Thread.currentThread().getName() + " start");
 
-            synchronized (object1){
+            synchronized (object2/*object1*/){
                 try {
                     System.out.println("thread1 -> object1");
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                synchronized (object2){
+                synchronized (object1/*object2*/){
                     System.out.println("thread1 -> object1 + object2");
                 }
             }
