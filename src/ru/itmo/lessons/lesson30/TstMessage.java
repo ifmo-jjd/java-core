2afde2cf780f4b05fa69fc32e8602077f773dd66
Dasha <byrtosha@yandex.ru>
2022-11-30 21:42:24 +0300

    Занятие 30: память ++

--- src/ru/itmo/lessons/lesson30/TstMessage.java
+++ src/ru/itmo/lessons/lesson30/TstMessage.java
@@ -1,16 +1,31 @@
 package ru.itmo.lessons.lesson30;
 
+import java.lang.ref.WeakReference;
+import java.util.HashMap;
+import java.util.WeakHashMap;
+
 public class TstMessage {
 
     public static void main(String[] args) {
+        // String[] args = []
         String data = "Срочное сообщение";
         int num = 10;
 
         printStart();
 
-        MemoryMessage message = new MemoryMessage();
+        MemoryMessage message = new MemoryMessage(); // сильная ссылка
         message.setText(data);
         message.setCode(num);
+
+        // слабая ссылка
+        WeakReference<MemoryMessage> weakReference = new WeakReference<>(message);
+        message = null;
+        // объект, у которого есть сильные ссылки не удаляется сборщиком мусора
+        // объект, у которого есть только слабые ссылки удаляется сборщиком мусора,
+        // если остается мало heap памяти
+
+        WeakHashMap<String, String> weakHashMap = new WeakHashMap<>();
+        HashMap<String, String> hashMap = new HashMap<>();
     }
 
     private static void printStart(){
